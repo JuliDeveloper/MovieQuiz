332e32cc9c7db2b00471d277e352b6da60942d9e
JuliDeveloper <rja-97@yandex.ru>
2022-09-17 17:53:58 +0300

    Реализовали все состояния и переходы MovieQuizViewController.

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -6,6 +6,22 @@ struct QuizQuestion {
   let correctAnswer: Bool
 }
 
+struct QuizStepViewModel {
+  let image: UIImage
+  let question: String
+  let questionNumber: String
+}
+
+struct QuizResultsViewModel {
+  let title: String
+  let text: String
+  let buttonText: String
+}
+
+struct QuizResultResponseViewModel {
+  let isCorrect: Bool
+}
+
 final class MovieQuizViewController: UIViewController {
     
     @IBOutlet private var imageView: UIImageView!
@@ -64,18 +80,90 @@ final class MovieQuizViewController: UIViewController {
             correctAnswer: false
         )
     ]
+    private var currentQuestionIndex: Int = 0
+    private var correctAnswers: Int = 0
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.layer.cornerRadius = 20
+        
+        show(quiz: convert(model: questions[currentQuestionIndex]))
     }
     
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
-
+        checkResponse(sender)
     }
 
     @IBAction private func noButtonClicked(_ sender: UIButton) {
+        checkResponse(sender)
+    }
+    
+    private func checkResponse(_ button: UIButton) {
+        if button.currentTitle == "Нет" {
+            let userResponse = QuizResultResponseViewModel(isCorrect: questions[currentQuestionIndex].correctAnswer ? false : true).isCorrect
+            showAnswerResult(isCorrect: userResponse)
+        } else {
+            let userResponse = QuizResultResponseViewModel(isCorrect: questions[currentQuestionIndex].correctAnswer ? true : false).isCorrect
+            showAnswerResult(isCorrect: userResponse)
+        }
+    }
+    
+    private func show(quiz step: QuizStepViewModel) {
+        imageView.image = UIImage(named: questions[currentQuestionIndex].image)
+        textLabel.text = "\(questions[currentQuestionIndex].text)"
+        counterLabel.text = "\(currentQuestionIndex + 1)/\(questions.count)"
+    }
 
+    private func show(quiz result: QuizResultsViewModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert)
+        let action = UIAlertAction(title: "Сыграть ещё раз", style: .default) { _ in
+            self.currentQuestionIndex = 0
+            self.correctAnswers = 0
+            self.show(quiz: self.convert(model: self.questions[self.currentQuestionIndex]))
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        return QuizStepViewModel(
+            image: UIImage(named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+    }
+    
+    private func showAnswerResult(isCorrect: Bool) {
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        
+        if isCorrect {
+            correctAnswers += 1
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQuestionOrResults()
+            self.imageView.layer.borderColor = .none
+        }
+    }
+    
+    private func showNextQuestionOrResults() {
+        if currentQuestionIndex == questions.count - 1 {
+            let text = "Ваш результат: \(correctAnswers) из \(questions.count)"
+            let viewModel = QuizResultsViewModel(
+                title: "Этот раунд окончен! ",
+                text: "\(text)",
+                buttonText: "Сыграть еще раз")
+            show(quiz: viewModel)
+        } else {
+            currentQuestionIndex += 1
+            show(quiz: convert(model: questions[currentQuestionIndex]))
+        }
     }
 }
 
