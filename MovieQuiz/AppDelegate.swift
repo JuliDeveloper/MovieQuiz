bfae3793abee71875ea2a3da4347bc9c2c321ba3
JuliDeveloper <rja-97@yandex.ru>
2022-09-17 20:18:41 +0300

    Удалила задержку для загрузки приложения

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -11,8 +11,6 @@ import UIKit
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        sleep(3)
-        print("Hello World")
         return true
     }
 
