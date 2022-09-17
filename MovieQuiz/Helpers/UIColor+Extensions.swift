2790e94093c74683feae868b8d5f9dac774db0e1
JuliDeveloper <rja-97@yandex.ru>
2022-09-17 17:49:36 +0300

    Исправила название цвета в расширении для UIColor

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -5,7 +5,7 @@ extension UIColor {
     static var ypBlack: UIColor { UIColor(named: "black")! }
     static var ypWhite: UIColor { UIColor(named: "white")! }
     static var ypDarkGray: UIColor { UIColor(named: "darkGray")! }
-    static var ypgray: UIColor { UIColor(named: "gray")! }
+    static var ypGray: UIColor { UIColor(named: "gray")! }
     static var ypRed: UIColor { UIColor(named: "red")! }
     static var ypGreen: UIColor { UIColor(named: "green")! }
 }
