75a30d6563fe8b1edf355bf254d00ef141a22277
JuliDeveloper <rja-97@yandex.ru>
2022-10-01 19:13:06 +0300

    SPRINT-5: Вынесла модели в отдельные файлы

---
+++ MovieQuiz/Models/QuizResultResponseViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  QuizResultResponseViewModel.swift
+//  MovieQuiz
+//
+//  Created by Julia Romanenko on 01.10.2022.
+//
+
+import Foundation
+
+struct QuizResultResponseViewModel {
+  let isCorrect: Bool
+}
